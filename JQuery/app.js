a69e45a84ce1557e542c3390b71dce8ac662ed93
unknown <jfrummel@gmail.com>
2018-09-27 16:20:59 -0500

    Finish

--- JQuery/app.js
+++ JQuery/app.js
@@ -6,3 +6,23 @@ $('#button').click(function() {
 $('#btn').click(function() {
     alert($('#text').val());
 });
+
+$('#div1').hover(
+    function() {
+      $( this ).addClass( "div" );
+    }, function() {
+      $( this ).removeClass( "div" );
+    }
+  );
+
+  $('#p1').click(function() {
+    let ranColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+    $(this).css('color', ranColor);
+  });
+
+  $('#friendsBtn').click(function() {
+    let friends = ['Bill', 'Bob', 'Mike', 'Andy', 'Joe', 'Trajan', 'Brian', 'Matt', 'AJ', 'Eric'];
+    friends.forEach(function(friend) {
+        $('#friendsList').append("<li>" + friend + "</li>");
+    });
+  });
