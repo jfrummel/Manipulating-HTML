1ec7690349cc3ad5e70655329e5a7de499aba075
unknown <jfrummel@gmail.com>
2018-09-27 16:22:04 -0500

    Done

--- JQuery/app.js
+++ JQuery/app.js
@@ -1,12 +1,14 @@
+//Alert with button
 $('<button id="button">Button 1</button>').appendTo('body');
 $('#button').click(function() {
     alert('Thanks for clicking');
 });
 
+//Alert with text from text area
 $('#btn').click(function() {
     alert($('#text').val());
 });
-
+//Change background color of div on hover
 $('#div1').hover(
     function() {
       $( this ).addClass( "div" );
@@ -15,14 +17,22 @@ $('#div1').hover(
     }
   );
 
+  //Random color to text of paragraph
   $('#p1').click(function() {
     let ranColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
     $(this).css('color', ranColor);
   });
+   
+  let array = ['Bill', 'Bob', 'Mike', 'Andy', 'Joe', 'Trajan', 'Brian', 'Matt', 'AJ', 'Eric'];
 
-  $('#friendsBtn').click(function() {
-    let friends = ['Bill', 'Bob', 'Mike', 'Andy', 'Joe', 'Trajan', 'Brian', 'Matt', 'AJ', 'Eric'];
-    friends.forEach(function(friend) {
-        $('#friendsList').append("<li>" + friend + "</li>");
+ 
+//Add friends in unordered list
+function myFriends() {
+    array.forEach(function(item) {
+    $('#friendsList').append('<li>' + item + '</li>');
+
     });
-  });
+
+  };
+$('#friendBtn').click(myFriends);
+
